dec7c8be968612e1de554f7e2122588dce0e9c9f
Tsuyoshi Ushio <ushio@simplearchitect.com>
2017-02-08 12:16:59 +0900

    Change package structure

--- deploy.bat
+++ deploy.bat
@@ -1,17 +1,17 @@
-copy apply.js ApplyTaskFolder
-copy general.js GeneralTaskFolder
-copy kubectl.js ApplyTaskFolder
-copy kubectl.js GeneralTaskFolder
-copy package.json ApplyTaskFolder 
-copy package.json GeneralTaskFolder
+copy apply.js tasks\apply
+copy general.js tasks\general
+copy kubectl.js tasks\apply
+copy kubectl.js tasks\general
+copy package.json tasks\apply 
+copy package.json tasks\general
 
-IF EXIST ApplyTaskFolder\node_modules ( 
-RD /s /q ApplyTaskFolder\node_modules 
+IF EXIST tasks\apply\node_modules ( 
+RD /s /q tasks\apply\node_modules 
 )
 
-IF EXIST GeneralTaskFolder\node_modules ( 
-RD /s /q GeneralTaskFolder\node_modules 
+IF EXIST tasks\general\node_modules ( 
+RD /s /q tasks\general\node_modules 
 )
 
-xcopy /Y /I /S node_modules ApplyTaskFolder\\node_modules
-xcopy /Y /I /S node_modules GeneralTaskFolder\\node_modules
+xcopy /Y /I /S node_modules tasks\\apply\\node_modules
+xcopy /Y /I /S node_modules tasks\\general\\node_modules
