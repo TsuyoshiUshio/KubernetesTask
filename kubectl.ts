00763c9a8a9499998e330f531adeae7f465e5953
rakelkar <rakelkar@microsoft.com>
2017-02-09 15:18:15 -0800

    add support to download kubectl

--- kubectl.ts
+++ kubectl.ts
@@ -19,7 +19,6 @@ export class KubectlCommand {
         this.kubeconfig = tl.getEndpointAuthorizationParameter(this.endpoint, 'kubeconfig', true);
         this.kubectlbinary = tl.getInput('kubectlBinary');
         this.configfile = path.join(tl.cwd(), "config");
-        this.kubectl = tl.tool(this.kubectlbinary);
     }
     append(arg) {
         this.kubectl.arg(arg);
@@ -27,18 +26,36 @@ export class KubectlCommand {
     exec() {
         this.execCommand();
     }
+    async init() {
+        if (!fs.exists(this.kubectlbinary)) {                
+            tl.debug("downloading kubectl");
+            let curl: ToolRunner = tl.tool("curl");
+            curl.arg("-LO").arg("https://storage.googleapis.com/kubernetes-release/release/v1.5.2/bin/linux/amd64/kubectl");
+            await curl.exec();
+            
+            tl.debug("settings kubectl exec perms");
+            let sudo: ToolRunner = tl.tool("chmod");
+            sudo.arg("777").arg("kubectl");
+            await sudo.exec();
+
+            this.kubectlbinary = "./kubectl";
+        }
+
+        this.kubectl = tl.tool(this.kubectlbinary);
+    }
     async execCommand() {
-        try {
-            tl.checkPath(this.kubectlbinary, 'kubectlBinary');
-            tl.debug("cwd(): " + tl.cwd());
-            tl.debug("configfile: " + this.configfile);
-            await fs.writeFile(this.configfile, this.kubeconfig);
-            this.kubectl.arg('--kubeconfig').arg('./config');
+        try {                     
+             tl.checkPath(this.kubectlbinary, 'kubectlBinary');
+             tl.debug("cwd(): " + tl.cwd());
+             tl.debug("configfile: " + this.configfile);
+             await fs.writeFile(this.configfile, this.kubeconfig);
+             this.kubectl.arg('--kubeconfig').arg(this.configfile);
 
-            await this.kubectl.exec();
+             tl.debug("settings kubectl exec perms");
+             await this.kubectl.exec();
 
-            tl.setResult(tl.TaskResult.Succeeded, "kubectl works.");
-            return;
+             tl.setResult(tl.TaskResult.Succeeded, "kubectl works.");
+             return;
         } catch (err) {
             tl.setResult(tl.TaskResult.Failed, err);
         }
