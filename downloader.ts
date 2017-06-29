140a4aa5764bb596bf7f0d5a171994fad92e1d68
Tsuyoshi Ushio <ushio@simplearchitect.com>
2017-06-29 16:46:45 +0900

    Implement Helm downloader and refacotring for it.
    I add the helm downloader. It causes duplication with istio downloader
    code. I refactor it first then I implement helm downloader.
    I should write a test code for this.

--- downloader.ts
+++ downloader.ts
@@ -8,23 +8,36 @@ import fs = require('fs');
 import { ToolRunner } from 'vsts-task-lib/toolrunner';
 let hasIstio: boolean = (tl.getInput('hasIstio') == 'true');
 let istioVersion: string = tl.getInput('IstioVersion');
+let hasHelm: boolean = (tl.getInput('hasHelm') == 'true');
+let helmVersion: string = tl.getInput('helmVersion');
 
 let kubectl: KubectlCommand = new KubectlCommand();
 
 function downloadIstioctl()  {
-        let istioBinaryDir = process.env['SYSTEM_DEFAULTWORKINGDIRECTORY'] + "/.vstsbin";
-        let bash = new ToolRunner(tl.which('bash', true));
-        let downloadURL = `https://github.com/istio/istio/releases/download/${istioVersion}/istio-${istioVersion}-linux.tar.gz`;
+        downloader(`https://github.com/istio/istio/releases/download/${istioVersion}/istio-${istioVersion}-linux.tar.gz`,
+                    "istioctl",
+                    "**/*/istioctl");
+}
 
-        let downloader = "curl -L " + downloadURL + " | tar xz" + "\n" + "cp **/*/istioctl " + istioBinaryDir;
-        let fileName = "./.istiodownloader.sh";
+function downloadHelm() {
+        downloader(`https://storage.googleapis.com/kubernetes-helm/helm-v${helmVersion}-linux-amd64.tar.gz`,
+                    "helm",
+                    "**/helm");
+}
+
+function downloader(downloadURL:string, binaryName:string, copyTarget:string)  {
+        let binaryDir = process.env['SYSTEM_DEFAULTWORKINGDIRECTORY'] + "/.vstsbin";
+        let bash = new ToolRunner(tl.which('bash', true));
+
+        let downloader = "curl -L " + downloadURL + " | tar xz" + "\n" + "cp " + copyTarget + " " + binaryDir;
+        let fileName = `./.${binaryName}downloader.sh`;
         fs.writeFileSync(fileName, downloader);
         bash.arg(fileName);
         try {
             bash.execSync();
         } catch(err) {
             tl.setResult(tl.TaskResult.Failed, err);
-            throw "Failed to exec the .istiodownloader.sh which is istio downloader."
+            throw `Failed to exec the .${binaryName}downloader.sh which is ${binaryName} downloader.`
         }
        
 }
@@ -33,6 +46,9 @@ kubectl.init().then(
     function() {
         if (hasIstio) {
             downloadIstioctl();
+        }
+        if (hasHelm) {
+            downloadHelm();
         }
     }
 );
