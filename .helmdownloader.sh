4a993b04637bc475a6fef177aa50694bf432f25e
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-07-13 17:11:22 -0700

    specify the helm directory to avoid helm directory problem

--- .helmdownloader.sh
+++ .helmdownloader.sh
@@ -1,2 +1,2 @@
 curl -L https://storage.googleapis.com/kubernetes-helm/helm-v2.4.2-linux-amd64.tar.gz | tar xz
-cp **/helm /opt/vsts/work/r1/a/.vstsbin
+cp ./linux-amd64/helm /opt/vsts/work/r1/a/.vstsbin
